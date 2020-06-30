62614f0f28788ef04ac3f0c12938a9adbde2cad4
陈柯 <chenke@julanling.com>
2020-06-30 18:09:47 +0800

    # 初始化

---
+++ src/main/java/cn/likegirl/lrpc/LrpcApplication.java
@@ -0,0 +1,13 @@
+package cn.likegirl.lrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LrpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/likegirl/lrpc/LrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.likegirl.lrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
